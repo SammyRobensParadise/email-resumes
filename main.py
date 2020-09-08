38a0fd2d7768df901ad538622033d1f7bacb2e23
Sammy Robens-Paradise <srobensparadise@gmail.com>
2020-09-07 23:37:06 -0400

    changes

--- main.py
+++ main.py
@@ -4,14 +4,31 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
+def makeMessage(frame, index):
+    contents = ''
+    contents = contents + "Hi " + frame['Incoming Student Name'][index] + "! \n"
+    contents = contents + " You've been matched with an upper-year SYDE student, " + frame['Upper Year Student Name'][
+        index] + "\n " \
+                 "who has volunteered to give you feedback on your resume!"
+    contents = contents + "I have done my best to pair you up with an upper year student" \
+                          "with experience in a field you are interested in" \
+                          "but it is possible it wont be an 'exact' match. With that being said," \
+                          " I am sure the feedback will be of value to you! \n\n"
+    contents = contents + "It is your responsibility to reach" \
+                          "out to your upper year via email (" + frame['Upper Year Student Email'][index] + \
+               ") to coordinate a REMOTE resume critique." \
+               " I suggest sending an initial copy of your resume" \
+               "Then meeting via Zoom or Google Meet / your preferred video conferencing tool" \
+               "To discuss feedback. If you would also like a website critique, please also send a link" \
+               " to the published site to your upper-year volunteer. \n\n"
+    contents
+
 
 def main():
-    df = pd.read_excel('')
-
+    df = pd.read_csv('/Users/sammyrobens-paradise/projects/email-resumes/Pairs.csv')
     smtp_server = 'mailservices.uwaterloo.ca'
 
     port = 465
-    # port = 587
     sender_email = ''
     username = input('username: ')
     password = input('password: ')
@@ -21,7 +38,12 @@ def main():
         server.login(username, password)
 
         for i in range(0, len(df.index)):
-            print("hello")
+            message_to_users = MIMEMultipart()
+            message_to_users['From'] = sender_email
+            message_to_users['To'] = df['Incoming Student Email'][i]
+            message_to_users['Cc'] = df['Upper Year Student Email'][i]
+            message_to_users['Subject'] = 'OFFICIAL - Your Resume Critique has a match!'
+            message_to_users.attach(MIMEText('Hi there'), 'plain')
 
 
 if __name__ == "__main__":
