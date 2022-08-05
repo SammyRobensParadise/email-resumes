1aa1f1dbe9e310b58d0e5e0bc8cff29b9e75ade6
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-08-04 21:08:36 -0700

    continued development

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -2,14 +2,16 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function EmailResumes({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </UserProvider>
   );
 }
 
-export default MyApp;
+export default EmailResumes;
--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,11 +1,45 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { getAccessToken, useUser } from '@auth0/nextjs-auth0';
+import { useUser } from '@auth0/nextjs-auth0';
 import { useState } from 'react';
 
 const Home: NextPage = () => {
   const [currentTab, setTab] = useState<'CRITIQUES' | 'COACHINGS'>('CRITIQUES');
   const { user } = useUser();
+
+  if (!user) {
+    return (
+      <div className='p-8 space-y-4'>
+        <div
+          className='bg-red-100 rounded-lg py-5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full'
+          role='alert'
+        >
+          <svg
+            aria-hidden='true'
+            focusable='false'
+            data-prefix='fas'
+            data-icon='exclamation-triangle'
+            className='w-4 h-4 mr-2 fill-current'
+            role='img'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 576 512'
+          >
+            <path
+              fill='currentColor'
+              d='M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z'
+            ></path>
+          </svg>
+          <p>
+            Unauthorized.{' '}
+            <Link href='/api/auth/login' passHref>
+              <a className='underline'>Login</a>
+            </Link>{' '}
+            to continue.
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className='p-8 space-y-4'>
@@ -66,10 +100,14 @@ const Home: NextPage = () => {
       </ul>
       <div className='tab-content'>
         {currentTab === 'CRITIQUES' && (
-          <div className='tab-pane fade show active'>Tab 1 content button version</div>
+          <div className='tab-pane fade show active'>
+            <p>Critiques where you will receive feedback on your resume</p>
+          </div>
         )}
         {currentTab === 'COACHINGS' && (
-          <div className='tab-pane fade'>Tab 2 content button version</div>
+          <div className='tab-pane fade'>
+            Critiques where you are providing feedback and coaching others
+          </div>
         )}
       </div>
     </div>
