28ebe0cc88d0398a8eaa57f020a646af83d9acce
Sammy Robens-Paradise <srobensparadise@gmail.com>
2022-08-18 10:52:44 -0700

    added new page

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -15,8 +15,9 @@ export default function Footer() {
       </div>
       <div className='flex space-x-2'>
         <a
-          type='button'
+          type='link'
           className='cursor-pointer inline-block px-6 py-2.5 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out'
+          href='mailto:srobensp@uwaterloo.ca?Subject=Email-Resumes%20Help'
         >
           Help
         </a>
